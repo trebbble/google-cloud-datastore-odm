60ea8acb8752093882c809cfde920900f55182a7
Chris Karvouniaris <christos.karvouniaris247@gmail.com>
2026-03-17 11:33:34 +0200

    linting

--- src/google_cloud_datastore_odm/__init__.py
+++ src/google_cloud_datastore_odm/__init__.py
@@ -1,6 +1,10 @@
 from .model import (
     Model as Model,
+)
+from .model import (
     field_validator as field_validator,
+)
+from .model import (
     model_validator as model_validator,
 )
 from .properties import (
